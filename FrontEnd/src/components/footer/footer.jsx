dab079613b8200388d6159999e26c31e2f4221a0
lucas <lucasmamelo@hotmail.com>
2026-03-10 16:44:07 -0300

    atualizando o footer

--- FrontEnd/src/components/footer/footer.jsx
+++ FrontEnd/src/components/footer/footer.jsx
@@ -11,14 +11,20 @@ export default function Footer() {
   return (
     <footer className="footer">
       <section className="footer-section">
-        <LinkedInIcon fontSize="large" />
-        <InstagramIcon fontSize="large" />
-        <FacebookIcon fontSize="large" />
-        <p>Tarraf Viva, APT34 T1 - Votuporanga 15503019 - São Paulo</p>
+
+        <nav className="social">
+          <ul>
+            <li><a href="#"><LinkedInIcon fontSize="large" sx={{ color:"white" }} /></a></li>
+            <li><a href="#"><InstagramIcon fontSize="large" sx={{ color:"white" }} /></a></li>
+            <li><a href="#"><FacebookIcon fontSize="large" sx={{ color:"white" }} /></a></li>
+          </ul>
+        </nav>
+
+        <p id="endereco">Tarraf Viva, APTO 34 T1 - Votuporanga 15503019 - São Paulo</p>
 
         <div className="contact">
-        <p className="contactItem"><LocalPhoneIcon fontSize="medium" sx={{ color:"white" }}/> (17) 99123-4567</p>
-        <p className="contactItem"><LocalPostOfficeIcon fontSize="medium" sx={{ color: "white" }}/> lucasmamelo@hotmail.com</p>
+          <p className="contactItem"><LocalPhoneIcon fontSize="medium" sx={{ color:"white" }}/> (21) 98033-2268</p>
+          <p className="contactItem"><LocalPostOfficeIcon fontSize="medium" sx={{ color: "white" }}/> lucasmamelo@hotmail.com</p>
         </div>
 
         <img src={logo} alt="Logo" />
